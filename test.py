f34830f80a33938614528b3320efaff9b026820a
王ing <144081493+Answer5664@users.noreply.github.com>
2026-05-29 16:46:18 +0800

    test
    test

---
+++ test.py
@@ -0,0 +1,17 @@
+import decimal
+
+print("woshinidie");
+a=0;
+b=0.5;
+c=0.50;
+print(type(a));
+print(type(b));
+print(type(c));
+
+
+print(10/4);
+print(10//4);
+print(10%4);
+print(2.6-1.2);#浮点数计算不精确
+print(round(2.6-1.2,3));#精确小数方法1
+print(decimal.Decimal(2.6)-decimal.Decimal(1.2));
