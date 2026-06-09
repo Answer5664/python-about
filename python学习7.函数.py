af1b33fb3ed40adc399c87d5755f67bd9cacaece
王ing <144081493+wangxinyu20020316@users.noreply.github.com>
2026-06-09 10:25:36 +0800

    Add files via upload

--- python学习7.函数.py
+++ python学习7.函数.py
@@ -1,11 +1,14 @@
 ##作业：自定义一个函数，传入一个参数（整数），判断这个参数是不是质数
 def prime_number(x):
-    for i in range(2,x):
-        if x%i==0:
-            print(f'{x}不是质数')
-            break
-    else:
-        print(f'{x}是质数')
+    if x<=1:
+        print(f'{x}不是质数')
+    else:    
+        for i in range(2,x):
+            if x%i==0:
+                print(f'{x}不是质数')
+                break
+        else:
+            print(f'{x}是质数')
 
 
 k=int(input('请输入你要判断的数字：'))
