f1064b011615c8930c5339690a1c08c4edbfc91f
王ing <144081493+wangxinyu20020316@users.noreply.github.com>
2026-06-09 10:07:54 +0800

    Add files via upload

---
+++ python学习8.面向对象.py
@@ -0,0 +1,43 @@
+##作业一：
+# 创建一个汽车类
+# 拥有的属性 品牌 价格 动力方式(油 电 混动) 百公里加速
+# 拥有的方法：当前某品牌的车，最高时速是多少，动力方式是什么
+##class Car:
+##    def __init__(self,brand,price,dlway,speed):
+##        self.brand=brand
+##        self.price=price
+##        self.dlway=dlway
+##        self.speed=speed
+##
+##    def __str__(self):
+##        return f'当前{self.brand}品牌的车，最高时速是{self.speed}，动力方式是{self.dlway}'
+##
+##    def __repr__(self):
+##        return f'当前{self.brand}品牌的车，最高时速是{self.speed}，动力方式是{self.dlway}'
+##
+##
+##a=Car('比亚迪','500000','混动','30km/s')
+##print(a.__str__())
+
+
+
+
+# 作业二 创建一个学生类
+# 属性 姓名 年龄 班级 学号
+# 方法 打印学生的自我介绍 包含所有信息
+##class Student:
+##    def __init__(self,name,age,class_name,number):
+##        self.name=name
+##        self.age=age
+##        self.class_name=class_name
+##        self.number=number
+##
+##    def __str__(self):
+##        return f'大家好，我叫{self.name}，今年{self.age}岁，来自{self.class_name}，学号是{self.number}'
+##
+##    def __repr__(self):
+##        return f'大家好，我叫{self.name}，今年{self.age}岁，来自{self.class_name}，学号是{self.number}'
+##
+##b=Student('小白','18','3班',2055010504)
+##print(b)
+    
